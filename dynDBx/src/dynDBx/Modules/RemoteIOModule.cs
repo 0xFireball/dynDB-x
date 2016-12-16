213b8bac130aae4be1b8ebed96eab1beb9819e28
0xFireball <0xFireball@outlook.com>
2016-12-16 10:05:28 -0800

    add dynamic request reader

---
+++ dynDBx/src/dynDBx/Modules/RemoteIOModule.cs
@@ -0,0 +1,20 @@
+﻿using dynDBx.Utilities;
+using Nancy;
+using System.Collections.Generic;
+
+namespace dynDBx.Modules
+{
+    public class RemoteIOModule : NancyModule
+    {
+        public RemoteIOModule() : base("/io")
+        {
+            Put("/{path*}", args =>
+            {
+                var path = (string)args.path;
+                var data = (IDictionary<string, dynamic>)Request.Form;
+                // Return data written
+                return Response.AsJsonNet(data);
+            });
+        }
+    }
+}
