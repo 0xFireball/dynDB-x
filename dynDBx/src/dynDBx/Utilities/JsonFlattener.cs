7b3a5a5f9644fd0b74b98f5d6bc563779659449e
0xFireball <0xFireball@outlook.com>
2016-12-17 20:28:48 -0800

    add flattener utility

---
+++ dynDBx/src/dynDBx/Utilities/JsonFlattener.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace dynDBx.Utilities
+{
+    public class JsonFlattener
+    {
+        /// <summary>
+        /// Flatten JObject - http://stackoverflow.com/a/35838986
+        /// </summary>
+        /// <param name="jObj"></param>
+        /// <returns></returns>
+        public Dictionary<string, string> FlattenJObject(JObject jObj)
+        {
+            IEnumerable<JToken> jTokens = jObj.Descendants().Where(p => p.Count() == 0);
+            Dictionary<string, string> flattenedDict = jTokens.Aggregate(new Dictionary<string, string>(), (properties, jToken) =>
+            {
+                properties.Add(jToken.Path, jToken.ToString());
+                return properties;
+            });
+            return flattenedDict;
+        }
+    }
+}
